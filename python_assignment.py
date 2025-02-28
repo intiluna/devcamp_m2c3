05eea4b97661d82fa8b410d06b4bb6ac606431bc
Inti Luna <inlua@alumni.uv.es>
2025-02-28 10:14:53 +0100

    ejercicio 1

---
+++ python_assignment.py
@@ -0,0 +1,13 @@
+# Exercise 1
+## Create a string, number, list, and boolean, each stored in their own variable.
+string_var = "Aprendiendo a programar con python"
+number_var = 100
+list_var = [1, 2, "text1", 4, ["lista2_elemento1", "lista2_elemento2"]]
+boolean_var = True
+
+print(string_var)
+print(number_var)
+print(list_var)
+print(boolean_var)
+
+
